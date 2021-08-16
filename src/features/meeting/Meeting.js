1873bddacdefe05d683c45ac0d34ae19a799f45a
Jimmy Ruan <jimmyruan12@gmail.com>
2021-08-16 03:20:51 -0400

    wip for range slider

--- src/features/meeting/Meeting.js
+++ src/features/meeting/Meeting.js
@@ -15,6 +15,56 @@ function convertUtcToLocalTimeFormatted(utcTimeStr) {
     return convertUtcToLocalTime(utcTimeStr).format('ddd DD-MMM-YYYY, hh:mm A')
 }
 
+function calendarRanger(meetings) {
+    const uniqueDates = []
+    let dateStr = null
+
+    // if(meetings){
+    //     for(let meeting in meetings){
+    //         dateStr = convertUtcToLocalTime(meeting.start_time)
+    //         if(! uniqueDates.includes(meeting.dateStr)){
+    //             uniqueDates.push(dateStr)
+    //         }
+    //     }
+    // }
+    //
+    // console.log('I am here at 33', uniqueDates);
+
+
+    return (
+        <div className={styles.calendar_ranger_wrapper}>
+            <h3>Calendar Selection Range</h3>
+            <div className={styles.calendar_ranger}>
+                <div>
+                    <label htmlFor="start_day">
+                        Starting Day:
+                    </label>
+                    <select id="start_day" name="start_day">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="end_day">
+                        Ending Day:
+                    </label>
+                    <select id="end_day" name="end_day">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="fiat">Fiat</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
 export function Meeting() {
   const currentMeetings = useSelector(selectCurrentMeetings);
   const dispatch = useDispatch();
@@ -84,6 +134,7 @@ export function Meeting() {
 
     <div>
       <h1>Meeting List</h1>
+        {calendarRanger()}
       <div className={styles.wrapper}>
         {meetingItems}
       </div>
